9b831da4c7226b5b407cacaf7b4b4425e605ad98
Jeremiah Orlewicz <reflekshn@hotmail.com>
2021-12-01 02:11:58 +0000

    Animate a sentence, revealing one character at a time as per the set delay amount

---
+++ typewriter.js
@@ -0,0 +1,14 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+let interval = 50;
+
+for (let index = 0; index < sentence.length; index++) {
+  if (index !== sentence.length - 1) {
+    setTimeout(() => process.stdout.write(sentence[index]), delay);
+  } else {
+    setTimeout(() => process.stdout.write(sentence[index] + '\n'), delay);
+  }
+  delay += 50;
+};
+
+
